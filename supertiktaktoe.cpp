e2e3980f99d5fa55cc33af432b7f8f20c74cf3f1
HannahHK0622 <125647512+HannahHK0622@users.noreply.github.com>
2023-11-03 00:31:58 +0800

    Restructure to fit OOP

--- supertiktaktoe.cpp
+++ supertiktaktoe.cpp
@@ -3,94 +3,22 @@
 #include <string>
 
 using namespace std;
-
-vector<int[9]> getBoard(){//Defined so that it can be collapsed
-    vector<int[9]> board = {
-        {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-         
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0},
-
-         {0, 0, 0,
-         0, 0, 0,
-         0, 0, 0}
-    }; 
-    return board;
-}
-
-void format(vector<int[9]>* game){
-    
-}
-
-int* takeInput(){
-    int inputs[2];
-    return inputs;
-}
-
-int* takeInput(int* lastInput){ //Overload to take last input
+class subGame{
+    int size;
+    int board[];
+    int subGameWinner;
 
 }
 
-vector<int[9]> makePlay(vector<int[9]>* game, int[2]& play){
-    return game;
+class Game: public subGame{
+    int size;
+    subGame subgames[];
+    int overallWinner;
 }
 
-int* getGameState(vector<int[9]>* game){
-    int[3] gameState;
-    return gameState;
-}
 
-int main(int argc, char** argv){
-    //Initialisation
-    bool endGame = false;
-    string* players[2];
-    cout << "Please type the name of Player 1";
-    cin >> players[0];
-    cout << endl << "Please type the name of Player 2";
-    cin >> players[1];
-    cout << endl;
 
-    //Initial play
-    vector<int[9]> game = getBoard();
-    format(game);
-    int play = takeInput();
-    vector<int[9]> game = makePlay(&game, play)
-    
-    //While loop for normal gameplay
-    while(endGame == false){
-        format(game);
-        int play[] = takeInput(play);
-        vector<int[9]> game = makePlay(&game, play);
-        int* gameState = getGameState(&game);
-    }
-    int winner = gameState[1];
-    cout << winner << " has won the game!" << endl;
+int main(){
+
     return 0;
 }
