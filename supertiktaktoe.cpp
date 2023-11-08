4102dbd18b6999973a4620a6be9470cc5c39bb9b
HannahHK0622 <125647512+HannahHK0622@users.noreply.github.com>
2023-11-08 17:13:03 +0800

    Added destructor functions for Game and subGame, changed the subgame[] array to be a pointer to array instead, and very limited game logic.

--- supertiktaktoe.cpp
+++ supertiktaktoe.cpp
@@ -8,6 +8,7 @@ private:
     size_t size;
     int* board; 
     int winner;
+    bool playable;
 
 public:
     void init(int size){
@@ -32,36 +33,107 @@ public:
     int getSize(){
         return this->size;
     }
+
+    bool getPlayable(){
+        return this->playable;
+    }
+
+    int* getBoard(){
+        return this->board;
+    }
+
+
+    void destruct(){
+        delete[] board;
+    }
+    //Jintao's work
 };
 
 class Game{
 
 private: 
     size_t size;
-    subGame* subgames;
+    subGame** subgamesPtr;
+    bool* playable;
+    int* subWinners;
+    bool gameOver;
     int winner;
 
 public: 
     void init(int size){
         this->size = size;
-        this->subgames = new subGame[size * size];
+        subGame* newSubgames = new subGame[size * size];
+        this->playable = new bool[size*size];
+        this->subgamesPtr = &newSubgames;
         for(int i = 0; i < size*size; i++){
-            subgames[i].init(size);
+            (*subgamesPtr)[i].init(size);
         }
     }
 
-    void playsMove(int row, int col, int player, int* lastPlay){
-        int board = convertMove(lastPlay[0], lastPlay[1], this->size);
-        subgames[board].playsMove(row, col, player); 
+    void playsMove(int row, int col, int player, int board){
+        
+    }
+    
+    void setOverallWinner(subGame** subgamePtr){
+
+
     }
 
-    subGame* getSubgames(){
-        return this->subgames;
+    subGame** getSubgamesPtr(){
+        return this->subgamesPtr;
     }
 
     int getSize(){
         return this->size;
     }
+
+    void rawDump(){
+        cout << "You are using a debug feature... Just so you know" << endl;
+        subGame** subgames = this->subgamesPtr;
+        size_t size = this->size;
+        for(int board = 0; board < size; board++){
+            for(int elem = 0; elem < size; elem++){
+                cout << (*subgames)[board].getBoard()[elem];
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    void formattedOutput(){
+        subGame** subgames = this->subgamesPtr;
+        size_t size = this->size;
+    }
+
+    void destruct(){
+        subGame** subgames = this->subgamesPtr;
+        size_t size = this->size;
+        for(int board = 0; board < size; board++){
+            (*subgames)[board].destruct();
+        }
+        delete[] subgamesPtr;
+        delete[] playable;
+        delete[] subWinners;
+    }
+
+    bool getGameOver(){
+        return this->gameOver;
+    }
+
+    bool getWinner(){
+        return this->winner;
+    }
+
+    void update(){
+        subGame** subgames = this->subgamesPtr;
+        size_t size = this->size;
+        for(int board = 0; board < size*size; board++){
+            this->playable[board] = (*subgames)[board].getPlayable();
+            this->subWinners[board]=(*subgames)[board].getWinner();
+        }
+        this->setOverallWinner(subgames); 
+    }
+    //Hannah's work
 };
 
 
@@ -69,29 +141,36 @@ int convertMove(int row, int col, int size){
     int convertedMove;
     //Some logic here...
     return convertedMove;
+
+    //Jintao's work
     }
-
-void printFormat(Game game){
-    subGame* subgames = game.getSubgames();
-}
-
-void printRaw(Game game){
-    subGame* subgames = game.getSubgames();
-    size_t size = game.getSize();
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            cout << subgames[i][j];
-        }
-        cout << endl;
-    }
-
-}
 
 void declareWinner(string winner){
     cout << "The player is " << winner << endl;
 }
 
+void debug(Game game){
+    cout << "debug active" << endl;
+    //Debug stuff goes here and please PLEASE delete before deadline
+    game.rawDump();
+}
+
+int chooseBoard(int* lastPlay, size_t size){
+    int board;
+    return board;
+}
+
+int* getDefaultLastPlay(){
+    //Gets the default value
+    int row=-1, col=-1; //{-1, -1} should be impossible, but it still fits int* so I guess?
+    int default[] = {row, col};
+    return default;
+}
+
 int main(){
+    const bool debugging = true;
+
+    int* lastPlay = getDefaultLastPlay();
     string player1, player2;
     string players[] = {player1, player2};
     char playerToken[] = {'X', 'O'};
@@ -103,7 +182,22 @@ int main(){
     Game game;
     game.init(size);
 
+    if(debugging){
+        debug(game);
+    }
+    
 
+    //Game logic begins here
+    while(true){
+        int board = chooseBoard(lastPlay, game.getSize());
+
+
+        if(game.getGameOver()){
+            break;
+        }
+    }
+    declareWinner(players[game.getWinner()]);
+    game.destruct();
     return 0;
 }
 
