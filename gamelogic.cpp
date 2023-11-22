dcfc4ce657ebad2d65cf18c5b48de0c13cdb418c
jintaoxie02 <u3593115@connect.hku.hk>
2023-11-22 13:28:06 +0800

    .

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 #include <string>
+#include "gamelogic.h"
+
 
 using namespace std;
+using namespace GameObj;
 
 int convertMove(int row, int col, int size){
     int convertedMove;
     //For a board of size "size", there are size^2 slots.
     //Unrelated, but maybe there's a way to express that:
     //1d coordinate = row*size + col?
+    //2d to 1d
     int convertedMove = row*size + col;
     return convertedMove;
 
     //Jintao's work
     }
 
-int chooseBoard(int* lastPlay, size_t size){
+int chooseBoard(int lastRow, int lastCol, size_t size, Game game){
     int board;
+    //a bunch of logic
+    //accoring to lastRow, lastCol, choose a board
+    //Also check if it's playable
+    //If the board is unplayable, return -1
+    
     return board;
 }
 
 int* getDefaultLastPlay(){
     //Gets the default value
     int row=-1, col=-1; //{-1, -1} should be impossible, but it still fits int* so I guess?
-    int default[] = {row, col};
-    return default;
+    int defaultValue[] = {row, col};
+    return defaultValue;
 }
 
 int* takeMove(bool freeBoardChoice){
@@ -52,4 +61,13 @@ bool checkInput(int* input, bool freeBoardChoice){
     return isValid;
 }
 
+
+/*
+[][][x] row 0 col 2
+[][][]
+[][][]
+
+
+[][][x][][][][][][] board[2]
+*/
     
